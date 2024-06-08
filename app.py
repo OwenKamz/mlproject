01ecd97dfadff83900fae4f324ac4466143823c5
OwenKamz <120659985+OwenKamz@users.noreply.github.com>
2024-06-08 20:15:10 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+# Load the trained model
+model = joblib.load('credit_approval_model.pkl')
+
+@app.route('/')
+def home():
+    return "Credit Card Approval Prediction API"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = model.predict([data['features']])
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
